242fcca15776887bf3a642c6d11cbc474ea17b92
Adam <adam@visibleone.com>
2024-12-02 01:29:55 +0630

    feat: create tech ass 9

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.js"],
+  theme: {
+    extend: {
+      screens: {
+        'xs': '480px',
+      },
+      colors: {
+        'orange': '#E63E21',
+        'orange-1': '#F35034',
+        'gray': '#C0C0C0',
+        'dark-gray': '#161616',
+        'dark-gray-1': '#262626',
+        'dark-gray-2': '#1B1B1B',
+        'dark-gray-3': '#2C2C2C',
+        'dark-gray-4': '#3F3E3E',
+      },
+    },
+  },
+  plugins: [],
+};
