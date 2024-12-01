2a464bf388769f445b39c484a0672c2eefc60b82
Adam <adam@visibleone.com>
2024-12-02 01:58:16 +0630

    fix: update footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         'orange': '#E63E21',
         'orange-1': '#F35034',
         'gray': '#C0C0C0',
+        'white-gray': '#FDFDFD',
         'dark-gray': '#161616',
         'dark-gray-1': '#262626',
         'dark-gray-2': '#1B1B1B',
